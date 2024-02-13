4cbb2cec7fe4255b7276355b358d1cfd0a209d5a
PC <PC@DESKTOP-EPJPAG7>
2024-02-13 21:12:48 +0200

    first commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import CustomerReviews from "./CustomerReviews";
+import Footer from "./Footer";
+import PopularProduct from "./PopularProduct";
+import Service from "./Service";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+
+export {
+    CustomerReviews,
+    Footer,
+    Hero,
+    PopularProduct,
+    Service,
+    SpecialOffer,
+    Subscribe,
+    SuperQuality,
+}
